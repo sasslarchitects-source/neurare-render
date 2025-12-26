f82e0504856e2817a090d17eba89fd463b1f9a74
sasslarchitects-source <sasslarchitects@gmail.com>
2025-12-26 11:18:52 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,7 @@
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body style={{ margin: 0, background: "black", color: "white" }}>
+      <body style={{ margin: 0 }}>
         {children}
       </body>
     </html>
