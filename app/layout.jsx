e74538236b4562a440733b1d8f28ba7df7e621a5
sasslarchitects-source <sasslarchitects@gmail.com>
2025-12-26 13:18:27 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,15 +2,13 @@ import "./globals.css";
 
 export const metadata = {
   title: "Neurare",
-  description: "Architecture & Design",
+  description: "Next.js App Router working correctly",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
