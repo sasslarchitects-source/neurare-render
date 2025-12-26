5fb746192737e03f4cf253cb7b137954fb18cc05
sasslarchitects-source <sasslarchitects@gmail.com>
2025-12-26 11:18:23 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,15 @@
 export default function Home() {
   return (
     <main style={{
-      height: "100vh",
+      minHeight: "100vh",
       display: "flex",
       alignItems: "center",
       justifyContent: "center",
+      background: "black",
+      color: "white",
       fontSize: "32px"
     }}>
-      🚀 Neurare Render Live
+      Neurare Render Live 🚀
     </main>
   );
 }
