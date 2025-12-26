540a4d63a0c24a5bed1aaa1a0382a829c83fd695
sasslarchitects-source <sasslarchitects@gmail.com>
2025-12-26 10:54:06 +0530

    Create page.jsx

---
+++ app/app/page.jsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main style={{
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontSize: "40px"
+    }}>
+      Neurare Render – Live 🚀
+    </main>
+  );
+}
