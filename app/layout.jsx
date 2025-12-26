b3e518881fd9d4e8e34c7695ab1b5d61e9435f2e
sasslarchitects-source <sasslarchitects@gmail.com>
2025-12-26 12:34:51 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,9 @@
+export const dynamic = "force-static";
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body style={{ margin: 0 }}>
-        {children}
-      </body>
+    <html>
+      <body>{children}</body>
     </html>
   );
 }
