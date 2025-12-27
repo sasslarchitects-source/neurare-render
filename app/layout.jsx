30a5af97d3461526009ba71df7694cd46002f203
sasslarchitects-source <sasslarchitects@gmail.com>
2025-12-27 10:15:21 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,20 @@
-import "./globals.css";
-
 export const metadata = {
-  title: "Neurare Architects",
-  description: "Architecture Studio Website",
+  title: "Neurare Render",
+  description: "3D Render AI Website",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        style={{
+          margin: 0,
+          padding: 0,
+          fontFamily: "Arial, sans-serif",
+        }}
+      >
+        {children}
+      </body>
     </html>
   );
 }
