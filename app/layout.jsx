4ff088f07df9d1476e449fa216c4275f1d70f0ff
sasslarchitects-source <sasslarchitects@gmail.com>
2025-12-27 10:27:11 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,18 +1,7 @@
-export const metadata = {
-  title: "Neurare Render",
-  description: "3D Render AI Website",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        style={{
-          margin: 0,
-          padding: 0,
-          fontFamily: "Arial, sans-serif",
-        }}
-      >
+      <body>
         {children}
       </body>
     </html>
